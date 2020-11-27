b26ab6ca1352ab014908fbb75a9d587cdd880052
Craig Smith <craig.smith@customd.com>
2020-11-27 17:04:17 +1300

    array values not keys

--- config/eloquent-model-encrypt.php
+++ config/eloquent-model-encrypt.php
@@ -8,11 +8,11 @@ return [
     ],
     'tables' => [
     ],
-    'publickey' => env('ELOQUENT_MODEL_GLOBAL_PUBLIC_KEY', \storage_path() . '/_certs/public.key'),
-    'privatekey' => env('ELOQUENT_MODEL_GLOBAL_PRIVATE_KEY', \storage_path() . '/_certs/private.key'),
+    'publickey'     => env('ELOQUENT_MODEL_GLOBAL_PUBLIC_KEY', \storage_path() . '/_certs/public.key'),
+    'privatekey'    => env('ELOQUENT_MODEL_GLOBAL_PRIVATE_KEY', \storage_path() . '/_certs/private.key'),
     'models' => [
-        'keystore', \CustomD\EloquentModelEncrypt\Model\Keystore::class,
-        'keystore_key', \CustomD\EloquentModelEncrypt\Model\KeystoreKey::class,
-        'rsa_key', \CustomD\EloquentModelEncrypt\Model\RsaKey::class,
+        'keystore'      => \CustomD\EloquentModelEncrypt\Model\Keystore::class,
+        'keystore_key'  => \CustomD\EloquentModelEncrypt\Model\KeystoreKey::class,
+        'rsa_key'       => \CustomD\EloquentModelEncrypt\Model\RsaKey::class,
     ]
 ];
