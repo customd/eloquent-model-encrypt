f3981d99d370b6cc214128d0f94f9e2e37b5d3b7
Craig Smith <craig.smith@customd.com>
2021-04-19 22:06:45 +0000

    feat(command): Console command to trigger encrypt
    eme:encrypt:model {model} will not trigger all the 
    records to be encrypted.

---
+++ src/Console/Commands/EncryptModel.php
@@ -0,0 +1,54 @@
+
+<?php
+
+namespace CustomD\EloquentModelEncrypt\Console\Commands;
+
+use Illuminate\Console\Command;
+use CustomD\EloquentModelEncrypt\Jobs\EncryptModelRecords;
+
+class EncryptModel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'eme:encrypt:model {model} {--c|chunk=500 : The number of records to import at a time}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Trigger encryption on your model records (skips already encrypted ones)';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $class = $this->argument('model');
+        $model = new $class();
+
+        $total = $model->withoutGlobalScopes()->count();
+
+        $this->info("Found $total Records to encrypt");
+
+        $skip = 0;
+        $chunk = $this->option('chunk');
+
+        while ($skip < $total) {
+            $records = $model->newQuery()->withoutGlobalScopes()->skip($skip)->take($chunk)->get();
+            if ($records->isNotEmpty()) {
+                EncryptModelRecords::dispatch($records);
+                $key = $records->last()->getKey();
+                $this->line('<comment>Encrypted [' . $class . '] models up to ID:</comment> ' . $key);
+            }
+            $skip += $chunk;
+        }
+        $this->info('All [' . $class . '] records have been encrypted.');
+    }
+}
