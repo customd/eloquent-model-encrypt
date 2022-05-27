6befc1205a26c2a6bf7ab403730c5474f609d195
Craig Smith <craig.smith@customd.com>
2022-05-28 09:45:27 +1200

    fix: contract methods

--- src/Contracts/Encryptable.php
+++ src/Contracts/Encryptable.php
@@ -3,5 +3,17 @@ namespace CustomD\EloquentModelEncrypt\Contracts;
 
 interface Encryptable
 {
+    /**
+     * @var array<mixed, mixed> $options
+     */
+    public function forceEncrypt(array $options = []);
+
+    public function getEncryptionEngine();
+
+    public function isEncryptable(string $key): bool;
+
+    public function isValueEncrypted(?string $value): bool;
+
+    public function getTableKeystoreReference(): string;
 
 }
